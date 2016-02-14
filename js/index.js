e85bd3362f04e4b0dbf34c9d8f4e3f3e1846835d
Vivienne Kay <vivkay8@gmail.com>
2016-02-14 09:03:56 -0500

    feat(js): add grad show date to clock

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-function getTimeRemaining(endtime){
+function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor( (t/1000) % 60 );
-  var minutes = Math.floor( (t/1000/60) % 60 );
-  var hours = Math.floor( (t/(1000*60*60)) % 24 );
-  var days = Math.floor( t/(1000*60*60*24) );
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
   return {
     'total': t,
     'days': days,
@@ -13,16 +13,29 @@ function getTimeRemaining(endtime){
   };
 }
 
-function initializeClock(id, endtime){
+function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
-  var timeinterval = setInterval(function(){
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
     var t = getTimeRemaining(endtime);
-    clock.innerHTML = 'days: ' + t.days + '<br>' +
-                      'hours: ' + t.hours + '<br>' +
-                      'minutes: ' + t.minutes + '<br>'
-                      'seconds: ' + t.seconds;
-    if(t.total<=0){
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
       clearInterval(timeinterval);
     }
-  },1000);
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
 }
+
+var deadline = new Date(Date.parse(new Date('April 29 2016 18:59:59 GMT+02:00')));
+initializeClock('clockdiv', deadline);
